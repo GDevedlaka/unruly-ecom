481d753c7be12d24916e9413fc9b3f65cfddcc17
George Devedlaka <gdevpri123@gmail.com>
2016-12-13 22:25:05 +0000

    Changed to products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  root 'teddies#index'
-  resources :teddies, only: [:index, :show]
+  root 'products#index'
+  resources :products, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
